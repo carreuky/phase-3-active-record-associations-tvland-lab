b125ffe4bdaee5cd58fba93ff355bea05887202c
carreuky <carreuky@gmail.com>
2022-08-29 11:26:36 +0300

    commit tree cf7cf854307a03f6c87cda39b796afb9338be7c7

--- db/schema.rb
+++ db/schema.rb
@@ -21,12 +21,20 @@ ActiveRecord::Schema.define(version: 6) do
     t.string "name"
     t.integer "actor_id"
     t.integer "show_id"
-    t.string "catchprase"
+    t.string "catchphrase"
   end
 
   create_table "networks", force: :cascade do |t|
     t.string "call_letters"
     t.integer "channel"
+  end
+
+  create_table "shows", force: :cascade do |t|
+    t.string "name"
+    t.string "day"
+    t.string "season"
+    t.string "genre"
+    t.integer "network_id"
   end
 
 end
